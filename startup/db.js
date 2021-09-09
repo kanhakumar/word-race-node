718122e2b75b55f67ce6527b745a1b71b8ab63f8
kanhakumar <b517018@iiit-bh.ac.in>
2021-09-09 13:05:34 +0530

    db url changed

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = function () {
-  //const DB_URL=config.get("DB_URL"); // Use this when DB_URL is present in environment
-  //   const DB_URL = "mongodb://word-race-mongo-service:27017/local"; // Use this when MongoDB is running in docker
-  const DB_URL = "mongodb://localhost:27017/word-race"; //Use this when MongoDB is running in your local system
+  const DB_URL = "mongodb+srv://mfs:mfs@cluster0.vm6sf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; 
 
   mongoose
     .connect(DB_URL, {
